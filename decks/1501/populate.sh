46eb1357e1b38488bc68e0ce695e31e98200f786
David Curie <david.a.curie@vanderbilt.edu>
2020-09-03 00:19:09 -0500

    Revised script to be called from inside decks/1501/

--- decks/1501/populate.sh
+++ decks/1501/populate.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Use this bash script to populate the "gh-pages branch" with content from the maintained "master branch".
-# Call this script with a file to populate from master branch.
+# Call this script from within decks/1501/ with a file to populate from master branch.
 #   - Ex: sh populate.sh Lab1.html
 #   - No error checking is done to determine that the parent file actually exists.
 
 file=$1
-git checkout master decks/1501/"$file"
+git checkout master "$file"
